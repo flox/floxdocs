1fa6ffa0faf1a875448a513eaeea5a8d39c6142b
Rok Garbas <rok@flox.dev>
2024-09-18 17:41:26 +0200

    Reintroduce check links script (#27)

--- check_links.sh
+++ check_links.sh
@@ -6,6 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 lychee $SCRIPT_DIR/site \
   -nv \
-  --exclude "file://" \
-  --exclude "s3://" \
-  --exclude "https://alpha.floxsdlc.com"
+  --remap "https://flox.dev/docs file://$PWD/site"
