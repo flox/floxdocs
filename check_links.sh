cec3544b5374af2da73c0d7881631e9c63853720
Rok Garbas <rok@flox.dev>
2024-09-18 17:39:47 +0200

    no need to sat base

--- check_links.sh
+++ check_links.sh
@@ -6,5 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 lychee $SCRIPT_DIR/site \
   -nv \
-  --base "file://$PWD/site" \
   --remap "https://flox.dev/docs file://$PWD/site"
