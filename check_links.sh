bea24019b4d75c9da5a771087bd95ef00c655082
Matthew Kenigsberg <matthew@floxdev.com>
2024-11-12 09:07:26 -0700

    chore: Release 1.3.4 (#73)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Rok Garbas <rok@flox.dev>
    Co-authored-by: flox automations <73946670+floxbot@users.noreply.github.com>
    Co-authored-by: FloxBot <bot@flox.dev>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Zach Mitchell <zmitchell@fastmail.com>

--- check_links.sh
+++ check_links.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 lychee $SCRIPT_DIR/site \
   -nv \
-  --remap "https://flox.dev/docs file://$PWD/site"
+  --remap "https://flox.dev/docs file://$PWD/site" \
+  --exclude "bash/manual/html_node" # don't check the bash manual
