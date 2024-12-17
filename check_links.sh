2564076d115aac452496ee38227cd1b483334e96
Brendan Magee <bmagee@gmail.com>
2024-12-17 16:23:53 +0000

    fix(check_links.sh): Exclude GNU Make docs

--- check_links.sh
+++ check_links.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Check links, excluding the bash manual and the docs for GNU Make (returns a 429)
 lychee $SCRIPT_DIR/site \
   -nv \
   --remap "https://flox.dev/docs file://$PWD/site" \
-  --exclude "bash/manual/html_node" # don't check the bash manual
+  --exclude "bash/manual/html_node" \
+  --exclude "https://www.gnu.org/software/make/"
