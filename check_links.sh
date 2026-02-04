42f1182d1be3b6030d5a37dc1215a6597482bbaf
Morgan Helton <morgan@flox.dev>
2026-02-04 10:56:03 -0600

    chore: add hashicorp docs to link check exclusion list

--- check_links.sh
+++ check_links.sh
@@ -5,11 +5,12 @@ set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Check links, excluding the bash manual and the docs for GNU Make (returns a 429)
-# Backblaze also 429's
+# Backblaze and HashiCorp also 429
 lychee $SCRIPT_DIR/site \
   -nv \
   --remap "https://flox.dev/docs file://$PWD/site" \
   --exclude "bash/manual/html_node" \
   --exclude "https://www.gnu.org/software/make/" \
   --exclude "https://github.com/flox/catalog-util" \
-  --exclude "https://www.backblaze.com/cloud-storage"
+  --exclude "https://www.backblaze.com/cloud-storage" \
+  --exclude "https://developer.hashicorp.com/terraform"
